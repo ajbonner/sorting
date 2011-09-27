e7e3006ffd380291f7e5c535c1ed0973bbbee375
Aaron Bonner <ajbonner@gmail.com>
2011-09-27 11:05:11 +0100

    Add java bubblesort implementation

---
+++ Bubblesort.java
@@ -0,0 +1,33 @@
+class Bubblesort {
+
+    public static void main(String[] args)
+    {
+        int numints = 10000;
+        int[] ints = new int[numints];
+        long start, end;
+
+        for (int i = 0; i < numints; i++) {
+            ints[i] = (int)(Math.random() * 100000 + 1);
+        }
+        System.out.println("Sorting " + numints + " integers");
+        start = System.currentTimeMillis();
+        Bubblesort.bubblesort(ints, numints);
+        end = System.currentTimeMillis();
+        System.out.printf("Sort took %.4f secs\n", ((double)(end - start) / 1000));
+    }
+
+    public static void bubblesort(int[] ints, int numints)
+    {
+        int tmp;
+
+        for (int i = numints; i-- >= 0; ) {
+            for (int j = 0; j < i; j++) {
+                if (ints[j] > ints[j+1]) {
+                    tmp = ints[j];
+                    ints[j] = ints[j+1];
+                    ints[j+1] = tmp;
+                }
+            }
+        }
+    }
+}
